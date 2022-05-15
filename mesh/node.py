82ab32e79a0fd7790d8778de78f706206ca4e18b
qzcad <s.choporoff@gmail.com>
2022-05-15 17:04:07 +0300

    Node class

---
+++ mesh/node.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+from enum import Enum
+from scipy.spatial import distance
+
+import numpy as np
+
+
+class NodeType(Enum):
+    UNDEFINED = -2
+    EXTERNAL = -1
+    BORDER = 0
+    INTERNAl = 1
+    FIXED = 2
+
+
+class Node:
+    def __init__(self, coords: Iterable[float], node_type: NodeType):
+        self._coords = np.array(coords, dtype=float)  # type: np.ndarray
+        self._node_type = node_type
+
+    @property
+    def coords(self) -> np.ndarray:
+        return self._coords
+
+    @coords.setter
+    def coords(self, c: Iterable[float]):
+        self._coords = np.array(c, dtype=float)
+
+    @property
+    def node_type(self):
+        return self._node_type
+
+    @node_type.setter
+    def node_type(self, nt: NodeType):
+        self._node_type = nt
+
+    @property
+    def x(self) -> float:
+        return self._coords[0] if len(self._coords) > 0 else 0.0
+
+    @x.setter
+    def x(self, v):
+        if len(self._coords) > 0:
+            self._coords[0] = v
+
+    @property
+    def y(self) -> float:
+        return self._coords[1] if len(self._coords) > 1 else 0.0
+
+    @y.setter
+    def y(self, v):
+        if len(self._coords) > 1:
+            self._coords[1] = v
+
+    @property
+    def z(self) -> float:
+        return self._coords[2] if len(self._coords) > 2 else 0.0
+
+    @z.setter
+    def z(self, v):
+        if len(self._coords) > 2:
+            self._coords[2] = v
+
+    def euclidean(self, node: Node):
+        return distance.euclidean(self._coords, node._coords)
+
+    def vector(self, to_node: Node):
+        return to_node._coords - self._coords
